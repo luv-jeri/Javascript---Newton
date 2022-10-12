ad83f229d25c2fcd943d619c89ec55464ab44b36
luv-jeri <luvvjeri@gmail.com>
2022-10-12 21:51:05 +0530

    -

--- javascript/one.js
+++ javascript/one.js
@@ -1,12 +1,6 @@
-import { cardGen as Card } from './card.js';
-
-const btn = document.getElementById('btn');
-const container = document.getElementById('container');
-const titleInput = document.getElementById('title-input');
-const detailsInput = document.getElementById('details-input');
-const imgInput = document.getElementById('img-input');
+const btn = document.querySelector('#btn');
+const ball = document.querySelector('#ball');
 
 btn.addEventListener('click', function () {
-  const card = Card(titleInput.value, detailsInput.value, imgInput.value);
-  container.append(card);
+  ball.classList.toggle('animate');
 });
