4d8847733dfdde0d2edf9a74fa05e1d9d564d933
luv-jeri <luvvjeri@gmail.com>
2022-10-17 21:28:41 +0530

    -

--- javascript/one.js
+++ javascript/one.js
@@ -1,6 +1,39 @@
-const btn = document.querySelector('#btn');
-const ball = document.querySelector('#ball');
+const h1 = document.querySelector('h1');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
 
-btn.addEventListener('click', function () {
-  ball.classList.toggle('animate');
-});
+const whatToDo = (event) => {
+  //   h1.textContent = event.target.value;
+  h1.textContent = input.value;
+};
+
+input.addEventListener('keyup', whatToDo);
+
+// button.addEventListener('click', (event) => {
+//     console.log(event)
+// })
+
+// const addEventListener = (name, cb) => {
+//   const event = {
+//     name: 'click',
+//     some: '...',
+//   };
+
+//   cb(event);
+// };
+
+// addEventListener('click', (e) => {
+//   console.log('clicked !!!' , e);
+// });
+
+// const setUpListener = (fun) => {
+//   fun({
+//     name: 'click',
+//   });
+// };
+
+// const whatToDo = (e) => {
+//   console.log('Do this....', e);
+// };
+
+// setUpListener(whatToDo);
